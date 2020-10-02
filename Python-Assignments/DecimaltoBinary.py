c7ff9edcbf783d8237e7c8515b1c427701d943fc
EgnaroDev2 <47803678+EgnaroDev2@users.noreply.github.com>
2020-10-02 13:03:41 -0700

    Create DecimaltoBinary.py

---
+++ Python-Assignments/DecimaltoBinary.py
@@ -0,0 +1,11 @@
+# We add any two number (which represents decimal) then return it as binary
+# Decimal (two added number) >>> Binary
+def add_binary(a,b):
+  while a+b >= 0:
+    decimal = a+b
+    remainder = [] #Use the array to convert it to binary (adding all remainder equal binary)
+    #binary = 
+    break
+
+    
+add_binary(120,1)
