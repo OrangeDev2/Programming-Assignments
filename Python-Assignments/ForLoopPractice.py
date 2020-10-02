1c137c256ae8978aafc9f503471a967b4339f80e
EgnaroDev2 <47803678+EgnaroDev2@users.noreply.github.com>
2020-10-02 13:03:47 -0700

    Create ForLoopPractice.py

---
+++ Python-Assignments/ForLoopPractice.py
@@ -0,0 +1,8 @@
+a = [1, 2, 34124312, 4] #sums of 10!!
+
+total = 0
+for i in a: #for variable in expression
+    total = total + i # sum += element
+print(total) #Print out of loop so only print final value lol.
+
+    
