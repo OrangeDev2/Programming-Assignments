7da0c0a496de1b0786d47f25a3dc574fe0c5c6af
EgnaroDev2 <47803678+EgnaroDev2@users.noreply.github.com>
2020-10-02 13:03:39 -0700

    Create Day0.py

---
+++ Python-Assignments/Day0.py
@@ -0,0 +1,20 @@
+# Saboor Malik
+# CSP 017
+# Sep 11, 2019; Conditional Statements
+
+myVal = 10
+myNum = 50
+
+#if myVal > myNum:
+#if myVal == myNum:
+
+if myVal < myNum:
+    print('Less than')
+#else if = elif
+elif (myVal==myNum):
+    print('Equal to')
+else:
+    print('Greater than')
+
+#continue
+
