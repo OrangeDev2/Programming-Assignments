35a955c2cb49ebc96e3533661f1118b719a1156d
EgnaroDev2 <47803678+EgnaroDev2@users.noreply.github.com>
2020-10-02 13:03:26 -0700

    Create AskForUserName.py

---
+++ Python-Assignments/AskForUserName.py
@@ -0,0 +1,9 @@
+# Saboor Malik
+# CSP 017
+# Variables 2.E
+
+#Exercise 2: Write a program
+#that uses input to prompt a user for their name and then welcomes them.
+Name = input("Your name is: ")
+
+print("Hello "+Name)
