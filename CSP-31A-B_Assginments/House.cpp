787d5fac5a9e55aa5b1d062b76f9748a050470e9
EgnaroDev2 <47803678+EgnaroDev2@users.noreply.github.com>
2020-10-01 23:27:48 -0700

    Update House.cpp

--- CSP-31A-B_Assginments/House.cpp
+++ CSP-31A-B_Assginments/House.cpp
@@ -7,85 +7,103 @@
 #include <string>
 using namespace std;
 
-// class House
-class House{
-    private:
-        // variables
-        string house_name;
-        string address;
-        // 3 rooms
-        int rooms1;
-        int rooms2;
-        int rooms3;
-
-        //instances of other classes.
-        //Address addressObj;
-        //Room roomsObj;
-
-    public:
-        // Use methods for input, although each method are in Address class and Room class.  Call the methods with the instances we declared.
-        //...
-        House(){
-            cout << "Enter the house name: ";
-            cin >> house_name;
-
-        }
-        
-
-
-};
-
-// Address Class
 class Address{
     private:
-        // Variables used for address.
-        int houseNum;
+        string house_number;
         string city;
         string state;
-
     public:
+        void addressGetDetails(){
+            cout << "Enter the house number: ";
+            getline(cin, house_number);
 
-        void address_Details(){
-            cout << "Enter the house number ";
-            cin >> houseNum;
-            cout << houseNum << endl;
+            cout << "Enter the city: ";
+            getline(cin, city);
 
-            cout << "Enter the city ";
-            cin >> city;
-            cout << city << endl;
+            cout << "Enter the state: ";
+            getline(cin, state);
+        }
 
-            cout << "Enter the state ";
-            cin >> state;
-            cout << state << endl;
+        void displayAddressDetails(string house_name){
+            cout << "Address is: " << house_number + " " << house_name + " " + city + ", " + state << "\n" << endl;
         }
 };
 
-// Room class
 class Room{
     private:
-        int length, width;
-        string name;
-
+        string room_length;
+        string room_width;
+        string room_name;
     public:
-        void roomDetails(){
-            cout << "Enter the name for room ";
-            cin >> name;
-            cout << name << endl;
+        void getRoomDetails(string room_number){
+            cout << "Enter the room name " + room_number + ": ";
+            getline(cin, room_name);
+            cout << "Enter the room length: ";
+            getline(cin, room_length);
+            cout << "Enter the room width: ";
+            getline(cin, room_width);
 
-            cout << "Enter the length ";
-            cin >> length;
-            cout << length << endl;
+            //cout << "Room Name: " << room_name << " , Length: " << room_length << " , Width: " << room_width << endl;
+        }
 
-            cout << "Enter the width ";;
-            cin >> width;
-            cout << width << endl;
-    }
+        void displayRoomDetails(){
+            cout << "Room Name: " << room_name << ", Length: " << room_length << ", Width: " << room_width << "\n" << endl;
+        }
 };
 
-// main function
+// House Class
+class House{
+    private:
+        // Private Variables as required by Rubric.  Done!
+        string house_name;
+        string address;
+        string room1;
+        string room2;
+        string room3;
+    
+    public:
+        Address obj; // object
+        Room obj1; // object for each room
+        Room obj2; // object for each room
+        Room obj3; // object for each room
+
+        House(){ // Constructor
+            // Get Details
+            cout << "Enter the house name: ";
+            getline(cin, house_name);
+
+            // Address Details
+            obj.addressGetDetails();
+
+            // Room Details
+            obj1.getRoomDetails("1"); // Passing parameters for easy display -> room 1, room 2, room 3.
+            obj2.getRoomDetails("2"); // room 2
+            obj3.getRoomDetails("3"); // room 3
+
+            /* Output */
+            // Output House name
+            cout << "\n" << endl;
+            cout << "Display House name: " << house_name << endl;
+
+            // Output Address
+            obj.displayAddressDetails(house_name);
+
+            // Output Room Details
+            cout << "Display Room Details 1:" << endl;
+            obj1.displayRoomDetails();
+            cout << "Display Room Details 2:" << endl;
+            obj2.displayRoomDetails();
+            cout << "Display Room Details 3:" << endl;
+            obj3.displayRoomDetails();
+
+        }
+
+};
+
+
 int main(){
-    // an instance of class House only.
-    House houseObj;
+    House obj; // Call constructor
 
     return 0;
 }
+
